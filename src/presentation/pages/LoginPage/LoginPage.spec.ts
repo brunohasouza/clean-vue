51b5cd0439e190e996b6285439c495dd082e5937
Bruno Souza <souza.brunoha@gmail.com>
2022-09-30 23:22:02 -0300

    test: ensure Login starts with submit button disabled

--- src/presentation/pages/LoginPage/LoginPage.spec.ts
+++ src/presentation/pages/LoginPage/LoginPage.spec.ts
@@ -4,10 +4,12 @@ import { mount } from '@vue/test-utils'
 import LoginPage from './LoginPage.vue'
 
 describe('Login Page', () => {
-  test('Should not render spinner and error on start', () => {
+  test('Should start with initial state', () => {
     const wrapper = mount(LoginPage)
     const formStatus = wrapper.findComponent({ name: 'FormStatus' })
+    const submitBtn = wrapper.find('button[type="submit"]')
 
     expect(formStatus.element.childElementCount).toBe(0)
+    expect(submitBtn.attributes().disabled).toBeDefined()
   })
 })
