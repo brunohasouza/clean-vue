fe605831c12056610b656243e17a0a45faeef82c
Bruno Souza <souza.brunoha@gmail.com>
2022-10-09 17:54:47 -0300

    test: add EmailValidation

--- src/validation/validators/required-field/required-field-validation.spec.ts
+++ src/validation/validators/required-field/required-field-validation.spec.ts
@@ -3,16 +3,19 @@ import { faker } from '@faker-js/faker'
 import { RequiredFieldValidation } from './required-field'
 import { RequiredFieldError } from '@/validation/errors'
 
+const makeSut = (): RequiredFieldValidation =>
+  new RequiredFieldValidation(faker.random.word())
+
 describe('RequiredFieldValidation', () => {
   test('Should return error if field is empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const sut = makeSut()
     const error = sut.validate('')
 
     expect(error).toEqual(new RequiredFieldError())
   })
 
   test('Should return null if field is not empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const sut = makeSut()
     const error = sut.validate(faker.random.words())
 
     expect(error).toBeNull()
